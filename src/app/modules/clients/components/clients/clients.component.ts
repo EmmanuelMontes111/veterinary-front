a9b84fd9c6b5fbf5b24a7342ef0d55d809fc3451
EmmanuelMontes111 <emmauelmontes111@gmail.com>
2024-03-20 00:58:05 -0500

    show table clients in front whit petition get

--- src/app/modules/clients/components/clients/clients.component.ts
+++ src/app/modules/clients/components/clients/clients.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { ClientsService } from 'src/app/modules/shared/services/clients/clients.service';
 
 @Component({
@@ -6,7 +7,7 @@ import { ClientsService } from 'src/app/modules/shared/services/clients/clients.
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
-export class ClientsComponent implements OnInit{
+export class ClientsComponent implements OnInit {
 
   private medicineService = inject(ClientsService);
 
@@ -14,14 +15,46 @@ export class ClientsComponent implements OnInit{
     this.getClients();
   }
 
-  getClients(): void {
+  displayedColumns: string[] = ['clientId', 'name', 'lastName', 'residenceAddress', 'phoneNumber', 'actions'];
+  dataSource = new MatTableDataSource<ClientElement>();
+
+  getClients() {
     this.medicineService.getClients()
-    .subscribe( (data:any) => {
+      .subscribe((data: any) => {
 
-      console.log("Respuesta de clientes: ", data);
-
-    }, (error: any) => {
-      console.log("error: ", error);
-    })
+        console.log("Respuesta de clientes: ", data);
+        this.processClientsResponse(data);
+      }, (error: any) => {
+        console.log("error: ", error);
+      })
   }
+
+  processClientsResponse(response: any) {
+
+    const dataClient: ClientElement[] = [];
+
+    if (response.metadata[0].code = "200") {
+
+      let listClient = response.clientResponse.clients;
+
+      listClient.forEach((element: ClientElement) => {
+        dataClient.push(element);
+      });
+
+      this.dataSource = new MatTableDataSource<ClientElement>(dataClient);
+      
+
+    }
+
+  }
+
+}
+
+export interface ClientElement {
+  id: number;
+  clientId: number;
+  name: string;
+  lastName: string;
+  residenceAddress: string;
+  phoneNumber: number;
 }
